75e19a92fce4023dadeedf30e26b107de24eee1b
Sara3394 <hailu.serafina@yahoo.com>
2022-06-02 08:47:32 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,10 @@
 	 * main - entry point
 	 *
 	 * Description: main prints a phrase
+	 *
 	 * Return:0 if exited successfully
 	 */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

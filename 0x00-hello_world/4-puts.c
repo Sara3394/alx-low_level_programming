f3b30c336f2dc75f359d2fa4e3ae1561d5278566
Sara3394 <hailu.serafina@yahoo.com>
2022-06-02 07:52:32 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main(void)
+
 	/**
 	 * main prints Programming is like building a multilingual puzzle
 	 * Return returns 0 if its a success otherwise non zero value if an error occurs
 	 */
+int main(void)
 {
 	char str1[] = "Programming is like building a multilingual puzzle";
 		puts(str1);

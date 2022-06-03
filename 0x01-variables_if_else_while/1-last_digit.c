bbfdb5df4f4c953c53cca6f4293802378db431fa
Sara3394 <hailu.serafina@yahoo.com>
2022-06-03 12:10:25 -0700

    Task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the last digit of the number stored in var n
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int n;
+	char last_digit[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%s %d %d\n", "Last digit of", n, n % 10);
+	return (0);
+}

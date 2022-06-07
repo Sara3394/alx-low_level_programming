0d715c590eaeff0ee86c87140be756fc2cb7ec1b
Sara3394 <hailu.serafina@yahoo.com>
2022-06-07 12:52:22 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * @n: integer input
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+
+{
+	int h;
+	int m;
+	int i;
+	int j;
+	int k;
+	int l;
+
+		for (h = 0; h < 24; h++)
+		{
+			i = h / 10;
+			j = h % 10;
+			for (m = 0; m < 60; m++)
+			{
+				k = m / 10;
+				l = m % 10;
+				_putchar('0' + i);
+				_putchar('0' + j);
+				_putchar(':');
+				_putchar('0' + k);
+				_putchar('0' + l);
+				_putchar('\n');
+			}
+		}
+}

40fc7c3fb795c11e61a7486a9592097ca0941cf9
Sara3394 <hailu.serafina@yahoo.com>
2022-06-07 10:43:07 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks if a letter is lowercase
+ * @c: letter to check
+ *
+ * Return: 1 if success 0 otherwise
+ */
+int _islower(int c);
+
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

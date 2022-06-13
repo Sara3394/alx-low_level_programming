731f0200c29f32e4c0b8d709246fb96d5a6e2576
Sara3394 <hailu.serafina@yahoo.com>
2022-06-13 11:53:51 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * _strcpy - copies a string pointed by src
- * @dest: 1st argument char type pointer
- * @src: 2nd argument char type pointer
+ * _strcpy - copies string pointed to by src to buffer pointed to by dest
+ * @src: source to copy
+ * @dest: destination of copy
  *
- * Return: void
+ * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < src[i]; i++)
-		dest[i] = '\0';
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
 	return (dest);
 }

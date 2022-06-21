6459747b6bf84688f7a9dbea586d9f10ca238aa8
Sara3394 <hailu.serafina@yahoo.com>
2022-06-21 13:01:10 -0700

    Task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strstr - function that locates substring
- * @haystack: string in which needle is checked in
- * @needle: substring to find in haystack
+ * _strstr - a function that locates substring
+ * @haystack: string to locate
+ * @needle: substring to locate
  *
- * Return: pointer to beinning of needle 
+ * Return: pointer to the beginning of needle or null if no match
  */
 char *_strstr(char *haystack, char *needle)
-{ 
-	int i,j;
-	
-	for (i = 0; haystack[i] != '\0'; i++
-	     {
-		     for (j = 0; needle[i + j] != needle[j])
-			     break;
-		     
-	     }
-	     if (!needle[j])
-	     return (&haystack[i]);
-	     }
-	     return(NULL);
-	     }
+{
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,41 @@
-#include "main.h"
-#include "stdio.h"
+#include "main.h"                                                                                                                       
 
-/**
- * print_chessboard - a function that prints the chessboard
- * @a: pointer to pieces to print
+                                                                                                                                        
+
+/**                                                                                                                                     
  *
- * Return: void
- */
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
+ *  * print_chessboard - board to print                                                                                                    
+ *
+ *   * @a: prints things                                                                                                                    
+ *
+ *    * Return:                                                                                                                  
+ *
+ *     */                                                                                                                                     
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
-	}
+void print_chessboard(char (*a)[8])                                                                                                     
+
+{                                                                                                                                       
+
+	        int i;                                                                                                                          
+
+		        int j;                                                                                                                          
+
+			                                                                                                                                        
+
+			        for (i = 0; i < 8; i++)                                                                                                         
+
+					        {                                                                                                                               
+
+							                for (j = 0; j < 8; j++)                                                                                                 
+
+										                {                                                                                                                       
+
+													                        _putchar(a[i][j]);                                                                                              
+
+																                }                                                                                                                       
+
+									                _putchar('\n');                                                                                                         
+
+											        }                                                                                                                               
+
 }

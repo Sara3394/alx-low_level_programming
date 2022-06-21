90291ae64f22e8095416326f01a32eeb5b7e8a53
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2022-06-21 00:33:27 -0700

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory space to be filled
- * @b: byte to fill with
- * @n: number of spaces to fill
+ * _memcpy - copies memory area
+ * @dest: destination of copy
+ * @src: memory area to copy
+ * @n: number of bytes to copy
  *
- * Return: pointer to memory area s
+ * Return: pointer to dest
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		*(dest + i) = *(src + i);
 	}
-	return (s);
+	return (dest);
 }

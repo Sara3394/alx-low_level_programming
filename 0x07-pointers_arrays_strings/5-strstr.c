189310b24d0547df7d308d60ffbccdcb69fbcacd
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2022-06-21 12:06:20 -0700

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,25 +9,17 @@
  * Return: pointer to beinning of needle 
  */
 char *_strstr(char *haystack, char *needle)
-{
-	unsigned int i = 0, j = 0;
-
-	while (haystack[i])
-	{
-		while (needle[j && (haystack[i] == needle[0]))
-		{
-			if (haystack[i + j] == needle[j])
-				j++;
-			else
-				break;
-		}
-		if (needle[j])
-		{
-			i++;
-			j = 0;
-		}
-		else
-			return (haystack + i);
-	}
-	return (0);
-}
+{ 
+	int i,j;
+	
+	for (i = 0; haystack[i] != '\0'; i++
+	     {
+		     for (j = 0; needle[i + j] != needle[j])
+			     break;
+		     
+	     }
+	     if (!needle[j])
+	     return (&haystack[i]);
+	     }
+	     return(NULL);
+	     }

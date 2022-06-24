5461fdbee4a7a67e19a4b4d0862f1a7ae2d31fca
Sara3394 <hailu.serafina@yahoo.com>
2022-06-24 15:22:23 -0700

    =Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -prints the sum given arguments
+ * @argc: no of arguments
+ * @argv: recieved arguments
+ * Return: 0 if sucessful 1 if the argument is not a digit
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+	int count;
+	int i;
+
+	count = 1;
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (count < argc)
+	{
+		for (i = 0; argv[count][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[count][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}

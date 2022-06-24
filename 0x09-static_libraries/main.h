9dd02645d793f97de8510608ac9a2f395aacb426
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2022-06-24 13:07:51 -0700

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,38 +1,38 @@
 int _putchar(char c);
 
-int _islower(int c);
+int _islower();
 
-int _isalpha(int c);
+int _isalpha();
 
-int _abs(int n);
+int _abs();
 
-int _isupper(int c);
+int _isupper();
 
-int _isdigit(int c);
+int _isdigit();
 
-int _strlen(char *s);
+int _strlen();
 
-void _puts(char *s);
+void _puts();
 
-char *_strcpy(char *dest, char *src);
+char *_strcpy();
 
-int _atoi(char *s);
+int _atoi();
 
-char *_strcat(char *dest, char *src);
+char *_strcat();
 
-char *_strncat(char *dest, char *src, int n);
+char *_strncat();
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy();
 
-int _strcmp(char *s1, char *s2);
+int _strcmp();
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset();
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy();
 
-char *_strchr(char *s, char c);
+char *_strchr();
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn();
 
 char *_strpbrk(char *s, char *accept);
 

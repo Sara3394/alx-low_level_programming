602fd5e888e38624ad328cf8cdf880daca57c199
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2022-06-28 07:43:45 -0700

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,26 +3,12 @@
 #include <stdlib.h>
 
 /**
-<<<<<<< HEAD
- *
- *  * free_grid  - frees up a grid
- *
- *   * @grid: grid to check
- *
- *    * @height: height of grid
- *
- *     */
-
-=======
  * free_grid  - frees up a grid
  * @grid: grid to check
  * @height: height of grid
  */
->>>>>>> e970e6abe56ab0f4f5b77f584deb5064f7e3fe85
 void free_grid(int **grid, int height)
-
 {
-<<<<<<< HEAD
 
 	        int i;
 
@@ -47,17 +33,4 @@ void free_grid(int **grid, int height)
 								                free(grid[i]);
 
 										        }
-=======
-        int i;
-
-        if (height <= 0)
-                return;
-
-        if (grid == NULL)
-                return;
-
-        for (i = 0; i < height; i++)
-        {
-                free(grid[i]);
-        }
->>>>>>> e970e6abe56ab0f4f5b77f584deb5064f7e3fe85
+     

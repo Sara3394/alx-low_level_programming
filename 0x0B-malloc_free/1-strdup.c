a557d6aa61b20c0fccbb99fc4fbf9b9018ab5bef
Sara3394 <hailu.serafina@yahoo.com>
2022-06-28 05:06:46 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - copies a string into a new buffer
+ * @str: the string to copy
+ * Return: pointer to te buffer
+ */
+char *_strdup(char *str)
+{
+	char *pt;
+	unsigned int l, i;
+
+	if (str == NULL)
+		return (NULL);
+	for (l = 0; str[l] != '\0'; l++)
+		l++
+			if (l < 1)
+				return (NULL);
+	pt = malloc(l * sizeof(char));
+	if (pt == NULL)
+	{
+		free(pt);
+		return (NULL);
+
+		for (i = 0; i < l; i++)
+			pt[i] = str[i];
+		pt[i] = '\0';
+		return (pt);
+	}

9b2669b0c4e74589ead3e12575c78162df009a1a
Sara3394 <hailu.serafina@yahoo.com>
2022-06-28 06:20:59 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 /**
-* str_concat - concatenates two strings
-* @str1: first string
-* @str2: second string
-* Return: pointer to a buffer containing str1 & str2
-*/
+ * str_concat - concatenates two strings
+ * @str1: first string
+ * @str2: second string
+ * Return: pointer to a buffer containing str1 & str2
+ */
 char *str_concat(char *str1, char *str2)
 {
 	char *p;
 	int str1count, str2count;
 	int sizeBuffer;
 	int i;
-	
+
 	if (str1 == NULL)
 		str1 = "";
-	if (str2 == NULL)
+	if (str2 == MULL)
 		str2 = "";
 	for (str1count = 0; str1[str1count]; str1count++)
 		;
-	for (str2count = 0; str2[str2count]; str2count++)
+	for(str2count = 0; str2[str2count]; str2count++)
 		;
 	sizeBuffer = str1count + str2count + 1;
 	p = malloc(sizeBuffer *sizeof(char));
 	if (p == NULL)
-		return (NULL);
+		return (NuLL);
 	for (i = 0; i < sizeBuffer; i++)
-		i < str1count ? (p[i] = str1[i]) : (p[i] = str2[i - str1count]);
+		i < str1count ? p[i] = str1[i] : (p[i] = str2[i - str1count]);
 	return (p);
 }

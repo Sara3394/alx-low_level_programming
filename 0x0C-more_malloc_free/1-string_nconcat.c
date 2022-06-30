6b1b575d5ad4eebfa450578d64462a2fea7ae004
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2022-06-30 09:56:03 -0700

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * string_nconcat - concatenates two strings upto n bytes
+ * string_nconcat - concatenate two strings up to n bytes.
  * @s1: source string
  * @s2: string to truncate up to n bytes
- * @n: no of bytes to truncate
- * Return: pointer to buffer
+ * @n: number of bytes to truncate by
+ * Return: pointer to new buffer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	for (s1count = 0; s1[s1count]; s1count++)
 		;
-	for (s2count =0; s2[s2count]; s2count++)
+	for (s2count = 0; s2[s2count]; s2count++)
 		;
 	s2count > n ? (s2count = n) : (n = s2count);
 	sizeBuffer = s1count + s2count + 1;
@@ -27,6 +27,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < sizeBuffer - 1; i++)
 		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-					p[sizeBuffer] = '\0';
-					return (p);
-					}
+	p[sizeBuffer] = '\0';
+	return (p);
+}

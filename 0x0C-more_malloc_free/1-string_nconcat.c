59c3b80581996a27f21b8802b84df5aec0edb2f2
Sara3394 <105051162+Sara3394@users.noreply.github.com>
2022-06-30 09:59:47 -0700

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,60 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "coding.h"
+int _strlen(char *s);
+
 /**
- * string_nconcat - concatenate two strings up to n bytes.
- * @s1: source string
- * @s2: string to truncate up to n bytes
- * @n: number of bytes to truncate by
- * Return: pointer to new buffer
+ * *string_nconcat - concatenates two strings
+ * @s1: string type
+ * @s2: string type
+ * @n: size of string
+ * Return: pointer to newly allocated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int s1count, s2count, sizeBuffer, i;
+	unsigned int i, len, count;
+	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (s1count = 0; s1[s1count]; s1count++)
-		;
-	for (s2count = 0; s2[s2count]; s2count++)
-		;
-	s2count > n ? (s2count = n) : (n = s2count);
-	sizeBuffer = s1count + s2count + 1;
-	p = malloc(sizeBuffer * sizeof(char));
-	if (p == NULL)
+
+	len = _strlen(s2);
+
+	if (n >= len)
+		ptr = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+	else
+		ptr = malloc(sizeof(char) * (_strlen(s1) + n + 1));
+	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < sizeBuffer - 1; i++)
-		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-	p[sizeBuffer] = '\0';
-	return (p);
+
+	count = 0;
+	while (s1[count] != '\0')
+	{
+		ptr[count] = s1[count];
+		count++;
+	}
+	i = 0;
+	while (s2[i] != '\0' && i < n)
+	{
+		ptr[count] = s2[i];
+		count++;
+		i++;
+	}
+	ptr[count] = '\0';
+	return (ptr);
 }
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The given string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int total;
+
+	for (total = 0; s[total] != '\0'; total++)
+		;
+	return (total);

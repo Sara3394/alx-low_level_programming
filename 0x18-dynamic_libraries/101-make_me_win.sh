158689f6369b752ecf0e59d3e43f4b6d1a04f6a7
Sara3394 <hailu.serafina@yahoo.com>
2022-09-06 02:08:07 -0700

    First commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /run/jackpot.so https://github.com/Sara3394/alx-low_level_programming/0x18-dynamic_libraries/jackpot.so
+wget -q -O /run/jackpot.so https://github.com/Sara3394/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=/run/jackpot.so
